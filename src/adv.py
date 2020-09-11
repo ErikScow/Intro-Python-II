4d370b3f1bffc9468d76dbe9a9a0d5ee466464e0
erikscow <erikscow@gmail.com>
2020-09-10 20:45:43 -0700

    added item class, started logic to pick up items

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,33 @@
 from room import Room
 from player import Player
+from item import Item
+
+items = {
+    'sword': Item('sword', 'a basic sword'),
+    'book': Item('book', 'a book that teaches how to use spells'),
+    'shield': Item('shield', 'used to block attacks'),
+}
+
+
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance", """North of you, the cave mount beckons"""),
+    'outside':  Room("Outside Cave Entrance", """North of you, the cave mount beckons""", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [items['sword']]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", [items['shield']]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", [items['book']]),
 }
 
 
@@ -50,14 +59,16 @@ room['treasure'].s_to = 'narrow'
 #
 # If the user enters "q", quit the game.
 
-Player1 = Player("You", 'outside')
+Player1 = Player("You", 'outside', [])
 
 def currentLocation():
     return f"Your current location is the {room[Player1.current_room].name}\n{room[Player1.current_room].description}"
 
 print(currentLocation())
 
-cmd = input('Please select your action\n[n] go north\n[w] go west\n[s] go south\n[e] go east\n[q] quit\n')
+cmd, *item = [str(x) for x in input('Please select your action\n[n] go north\n[w] go west\n[s] go south\n[e] go east\n[search] search the room\n[q] quit\n').split()]
+
+#cmd, item = input('Please select your action\n[n] go north\n[w] go west\n[s] go south\n##[e] go east\n[search] search the room\n[q] quit\n')
 
 while not cmd == 'q':
     if cmd == 'n' or cmd == 'w' or cmd == 's' or cmd == 'e':
@@ -89,10 +100,22 @@ while not cmd == 'q':
             except AttributeError:
                 print('Nothing that way! Pick a different direction!')
                 pass
-    elif cmd == 'other inputs not yet valid':
-        print('These commands dont do anything yet, wait for future updates')
+    elif cmd == 'search':
+        itemsInRoom = ' '.join([str(elem) for  elem in room[Player1.current_room].items])
+        print(f"This room contains the following items:", itemsInRoom, sep='\n')
+    elif cmd == 'inventory':
+        itemsInInventory = ' '.join([str(elem) for  elem in Player1.items])
+        print(f"You are holding the following items:", itemsInInventory, sep='\n')
+    elif cmd == 'take':
+        itemsInRoom = room[Player1.current_room].items
+        for i in itemsInRoom:
+            if i.name == item:
+                Player1.add_item(i)
+                print(f"You picked up the {item}!")
+            else:
+                print("That item is not in this room")
     else:
         print('Please enter a valid command')
 
-    cmd = input('Please select your action\n[n] go north\n[w] go west\n[s] go south\n[e] go east\n[q] quit\n')
+    cmd, *item = [str(x) for x in input('Please select your action\n[n] go north\n[w] go west\n[s] go south\n[e] go east\n[search] search the room\n[q] quit\n').split()]
         
--- src/player.py
+++ src/player.py
@@ -1,12 +1,23 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
+from item import Item
+
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, items = []):
         self.name = name
         self.current_room = current_room
+        self.items = items
     def __str__(self):
-        return f"name: {self.name}\ncurrent room: {self.current_room}"
+        return f"name: {self.name}\n    current room: {self.current_room}"
 
     def change_room(self, new_room):
         self.current_room = new_room
+
+    def add_item(self, itemAdded):
+        self.items.append(itemAdded)
+
+    def drop_item(self, itemDropped):
+        for item in self.items:
+            if item.name == itemDropped.name:
+                del self.items[item]
